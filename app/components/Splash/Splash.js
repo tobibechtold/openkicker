692ad914d70d1640d27d3e5daedc4d1c6b3383a0
Tobi <tobi.bechtold@gmail.com>
2018-08-31 23:19:10 +0200

    added splash screen image

--- app/components/Splash/Splash.js
+++ app/components/Splash/Splash.js
@@ -3,12 +3,14 @@ import { View, Text, ActivityIndicator, Image } from 'react-native';
 
 import styles from './styles'
 
+const logo = require('../../assets/img/logo.jpg')
+
 export default class extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.wrapper}>
-                    <Image style={styles.image} source={{uri: ""}}/>
+                    <Image style={styles.image} source={logo}/>
                     <Text style={styles.title}>openkicker</Text>
                 </View>
                 <View style={styles.activityIndicatorContainer}>
